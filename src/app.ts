8c51439af452044feb9de46bec326595534112d7
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-06-27 00:35:32 +0700

    init project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import connectMongooDB from './config/connect'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+connectMongooDB(String(process.env.DB_URL))
+
+
+export const viteNodeApp = app
+
